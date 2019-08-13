4374c14aa48eb7995d9cd2398175d5ba87ab4f86
wally <u.str.gm@gmail.com>
2019-08-13 20:34:41 +0900

    feat: viewの大枠を作成した

---
+++ components/Seats.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="line">
+    <div class="pair">
+      <v-card width="122" class="mx-auto">
+        <v-card-title>{{members[0].id}}</v-card-title>
+        <v-card-text>I'm card text</v-card-text>
+      </v-card>
+      <v-card width="122" class="mx-auto">
+        <v-card-title>{{members[1].id}}</v-card-title>
+        <v-card-text>I'm card text</v-card-text>
+      </v-card>
+    </div>
+    <div class="pair">
+      <v-card width="122" class="mx-auto">
+        <v-card-title>{{members[2].id}}</v-card-title>
+        <v-card-text>I'm card text</v-card-text>
+      </v-card>
+      <v-card width="122" class="mx-auto">
+        <v-card-title>{{members[3].id}}</v-card-title>
+        <v-card-text>I'm card text</v-card-text>
+      </v-card>
+    </div>
+    <div class="pair">
+      <v-card width="122" class="mx-auto">
+        <v-card-title>{{members[4].id}}</v-card-title>
+        <v-card-text>I'm card text</v-card-text>
+      </v-card>
+      <v-card width="122" class="mx-auto">
+        <v-card-title>{{members[5].id}}</v-card-title>
+        <v-card-text>I'm card text</v-card-text>
+      </v-card>
+    </div>
+    <div class="pair">
+      <v-card width="122" class="mx-auto">
+        <v-card-title>{{members[6].id}}</v-card-title>
+        <v-card-text>I'm card text</v-card-text>
+      </v-card>
+      <v-card width="122" class="mx-auto">
+        <v-card-title>{{members[7].id}}</v-card-title>
+        <v-card-text>I'm card text</v-card-text>
+      </v-card>
+    </div>
+    <div class="pair">
+      <v-card width="122" class="mx-auto">
+        <v-card-title>{{members[8].id}}</v-card-title>
+        <v-card-text>I'm card text</v-card-text>
+      </v-card>
+      <v-card width="122" class="mx-auto">
+        <v-card-title>{{members[9].id}}</v-card-title>
+        <v-card-text>I'm card text</v-card-text>
+      </v-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    members: {
+      required: true,
+      type: Array,
+      defualt: []
+    }
+  }
+};
+</script>
+
+<style scoped>
+.pair {
+  width: 244px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.line {
+  max-width: 1440px;
+  margin: 32px auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,70 +1,65 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <div class="text-xs-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline"
-          >Welcome to the Vuetify + Nuxt.js template</v-card-title
-        >
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a href="https://vuetifyjs.com" target="_blank">documentation</a>.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat"
-              >discord</a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-              >issue board</a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a href="https://nuxtjs.org/" target="_blank">Nuxt Documentation</a>
-          <br />
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank"
-            >Nuxt GitHub</a
-          >
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" flat nuxt to="/inspire">Continue</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <div>
+    <div style="width: 1360px; margin: 60px auto;">
+      <Seats :members="line1" />
+      <Seats :members="line2" />
+      <Seats :members="line3" />
+    </div>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
+import Seats from "~/components/Seats.vue";
 export default {
   components: {
-    Logo,
-    VuetifyLogo
+    Seats: Seats
+  },
+  data() {
+    return {
+      members: [
+        { id: 1, name: "abc" },
+        { id: 2, name: "abc" },
+        { id: 3, name: "abc" },
+        { id: 4, name: "abc" },
+        { id: 5, name: "abc" },
+        { id: 6, name: "abc" },
+        { id: 7, name: "abc" },
+        { id: 8, name: "abc" },
+        { id: 9, name: "abc" },
+        { id: 10, name: "abc" },
+        { id: 11, name: "abc" },
+        { id: 12, name: "abc" },
+        { id: 13, name: "abc" },
+        { id: 14, name: "abc" },
+        { id: 15, name: "abc" },
+        { id: 16, name: "abc" },
+        { id: 17, name: "abc" },
+        { id: 18, name: "abc" },
+        { id: 19, name: "abc" },
+        { id: 20, name: "abc" },
+        { id: 21, name: "abc" },
+        { id: 22, name: "abc" },
+        { id: 23, name: "abc" },
+        { id: 24, name: "abc" },
+        { id: 25, name: "abc" },
+        { id: 26, name: "abc" },
+        { id: 27, name: "abc" },
+        { id: 28, name: "abc" },
+        { id: 29, name: "abc" },
+        { id: 30, name: "abc" }
+      ]
+    };
+  },
+  computed: {
+    line1() {
+      return this.members.slice(0, 10);
+    },
+    line2() {
+      return this.members.slice(10, 20);
+    },
+    line3() {
+      return this.members.slice(20, 30);
+    }
   }
-}
+};
 </script>
