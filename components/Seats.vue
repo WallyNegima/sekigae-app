a3f435ec423cb78ce9d20a6e75f79448980b1132
wally <u.str.gm@gmail.com>
2019-08-13 20:59:04 +0900

    chore: 名前入力した

--- components/Seats.vue
+++ components/Seats.vue
@@ -3,51 +3,51 @@
     <div class="pair">
       <v-card width="122" class="mx-auto">
         <v-card-title>{{members[0].id}}</v-card-title>
-        <v-card-text>I'm card text</v-card-text>
+        <v-card-text>{{members[0].name}}</v-card-text>
       </v-card>
       <v-card width="122" class="mx-auto">
         <v-card-title>{{members[1].id}}</v-card-title>
-        <v-card-text>I'm card text</v-card-text>
+        <v-card-text>{{members[1].name}}</v-card-text>
       </v-card>
     </div>
     <div class="pair">
       <v-card width="122" class="mx-auto">
         <v-card-title>{{members[2].id}}</v-card-title>
-        <v-card-text>I'm card text</v-card-text>
+        <v-card-text>{{members[2].name}}</v-card-text>
       </v-card>
       <v-card width="122" class="mx-auto">
         <v-card-title>{{members[3].id}}</v-card-title>
-        <v-card-text>I'm card text</v-card-text>
+        <v-card-text>{{members[3].name}}</v-card-text>
       </v-card>
     </div>
     <div class="pair">
       <v-card width="122" class="mx-auto">
         <v-card-title>{{members[4].id}}</v-card-title>
-        <v-card-text>I'm card text</v-card-text>
+        <v-card-text>{{members[4].name}}</v-card-text>
       </v-card>
       <v-card width="122" class="mx-auto">
         <v-card-title>{{members[5].id}}</v-card-title>
-        <v-card-text>I'm card text</v-card-text>
+        <v-card-text>{{members[5].name}}</v-card-text>
       </v-card>
     </div>
     <div class="pair">
       <v-card width="122" class="mx-auto">
         <v-card-title>{{members[6].id}}</v-card-title>
-        <v-card-text>I'm card text</v-card-text>
+        <v-card-text>{{members[6].name}}</v-card-text>
       </v-card>
       <v-card width="122" class="mx-auto">
         <v-card-title>{{members[7].id}}</v-card-title>
-        <v-card-text>I'm card text</v-card-text>
+        <v-card-text>{{members[7].name}}</v-card-text>
       </v-card>
     </div>
     <div class="pair">
       <v-card width="122" class="mx-auto">
         <v-card-title>{{members[8].id}}</v-card-title>
-        <v-card-text>I'm card text</v-card-text>
+        <v-card-text>{{members[8].name}}</v-card-text>
       </v-card>
       <v-card width="122" class="mx-auto">
         <v-card-title>{{members[9].id}}</v-card-title>
-        <v-card-text>I'm card text</v-card-text>
+        <v-card-text>{{members[9].name}}</v-card-text>
       </v-card>
     </div>
   </div>
