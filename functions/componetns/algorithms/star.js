6e74ce1984b59f350afa68d1c4d621a440f8815c
高木颯汰 <kozamurai882@gmail.com>
2019-08-15 23:08:37 +0900

    serverからの返却値をrank順に並び替え / membersにrankと星座を追加

--- functions/componetns/algorithms/star.js
+++ functions/componetns/algorithms/star.js
@@ -1,30 +1,30 @@
 const axios = require('axios');
+const _ = require('lodash');
 
 const dateUtil = require('../utils/date');
+const arrayUtil = require('../utils/array');
 const db = require('../db');
 
 module.exports = {
     getMemberByStar: function () {
-        return {
-            "star": db.getStar(),
-            "members": db.getStarMember(),
-        };
-        // return axios.get('http://api.jugemkey.jp/api/horoscope/free/' + dateUtil.getToday())
-        //     .then(res => {
-        //         return parseStarJson(res.data);
-        //     })
-        //     .catch(err => {
-        //         console.error(err);
-        //         throw err
-        //     })
+        return axios.get('http://api.jugemkey.jp/api/horoscope/free/' + dateUtil.getToday())
+            .then(res => {
+                return parseStarJson(res.data);
+            })
+            .catch(err => {
+                console.error(err);
+                throw err
+            })
     }
 };
 
 const parseStarJson = (starObject) => {
     const startArray = starObject.horoscope[dateUtil.getToday()];
-    const returnObject = {
-        'star': startArray,
-        'members': db.getMembers()  // todo: 占い結果で並び替え
+    const exchangedStar = arrayUtil.exchangeStarResponse(startArray);
+    const members = db.getMembers();
+    arrayUtil.insertRankToMembers(exchangedStar, members);
+    return {
+        'star': _.sortBy(startArray, ['rank']),
+        'members': _.sortBy(members, ['rank'])
     };
-    return returnObject;
 };
--- functions/componetns/utils/array.js
+++ functions/componetns/utils/array.js
@@ -5,5 +5,18 @@ module.exports = {
             exchangedMembers.push(members[idx])
         });
         return exchangedMembers
+    },
+    exchangeStarResponse: function (objectArray) {
+        let returnObject = {};
+        for (let object of objectArray) {
+            returnObject[object.sign] = object;
+        }
+        return returnObject;
+    },
+    insertRankToMembers: function (stars, members) {
+        for (let member of members) {
+            member.rank = stars[member.star].rank;
+        }
+        return members;
     }
 };
