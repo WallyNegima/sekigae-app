ea064e3e180ac9e6ff7cf8f62220060fa4396154
wally <u.str.gm@gmail.com>
2019-08-15 13:00:51 +0900

    feat: 一旦表示！

--- components/Star.vue
+++ components/Star.vue
@@ -1,13 +1,28 @@
 <template>
-  <div>
-    <v-list-item-content v-for="star in stars" :key="star.sign">
-      <v-list-item-title>{{ star.sign }}</v-list-item-title>
-      <v-list-item-subtitle>{{ star.color }}</v-list-item-subtitle>
-    </v-list-item-content>
+  <div class="con">
+    <v-card
+      light
+      class="mx-auto card"
+      max-width="400"
+      tile
+      v-for="star in sortedStars"
+      :key="star.sign"
+    >
+      <v-list-item-content>
+        <v-list-item-title>{{star.rank}}位: {{ star.sign }}</v-list-item-title>
+        <v-list-item-subtitle>
+          ラッキーカラー: {{ star.color }}
+          <br />
+          一言: {{ star.content }}
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-card>
   </div>
 </template>
 
 <script>
+import _ from "lodash";
+
 export default {
   name: "Star",
   props: {
@@ -17,6 +32,16 @@ export default {
       default: () => {}
     }
   },
-  computed: {}
+  computed: {
+    sortedStars() {
+      return _.sortBy(this.stars, ["rank"]);
+    }
+  }
 };
 </script>
+
+<style scoped>
+.card {
+  margin: 16px auto;
+}
+</style>
